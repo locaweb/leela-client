4a824a76d435b3e898915a8983a4479635357344
PotHix <pothix@pothix.com>
2012-08-17 17:34:16 -0300

    Genarating a minified version for flotr2 and highcharts.

--- src/scripts/leela-widget-compress.sh
+++ src/scripts/leela-widget-compress.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-cat src/javascript/widget/functions.js src/javascript/widget/widget.js src/javascript/widget/version.js |\
-   java -jar src/scripts/yuicompressor-2.4.7.jar --type js -o src/javascript/widget/leela-widget.min.js
+cat src/javascript/widget/functions.js src/javascript/widget/widget.js src/javascript/widget/flotr2_backend.js src/javascript/widget/version.js |\
+   java -jar src/scripts/yuicompressor-2.4.7.jar --type js -o src/javascript/widget/leela-widget-flotr2.min.js
+
+cat src/javascript/widget/functions.js src/javascript/widget/widget.js src/javascript/widget/highcharts_backend.js src/javascript/widget/version.js |\
+   java -jar src/scripts/yuicompressor-2.4.7.jar --type js -o src/javascript/widget/leela-widget-highcharts.min.js
