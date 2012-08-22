f8849f933fcb657a271802f011329fb90e5a5a8e
diego souza <dsouza@c0d3.xxx>
2012-08-22 00:08:52 -0300

    bugfix: also updating leela-widget.min.js

--- src/scripts/leela-widget-compress.sh
+++ src/scripts/leela-widget-compress.sh
@@ -5,3 +5,6 @@ cat src/javascript/widget/functions.js src/javascript/widget/widget.js src/javas
 
 cat src/javascript/widget/functions.js src/javascript/widget/widget.js src/javascript/widget/highcharts_backend.js src/javascript/widget/version.js |\
    java -jar src/scripts/yuicompressor-2.4.7.jar --type js -o src/javascript/widget/leela-widget-highcharts.min.js
+
+cat src/javascript/widget/functions.js src/javascript/widget/widget.js src/javascript/widget/version.js |\
+   java -jar src/scripts/yuicompressor-2.4.7.jar --type js -o src/javascript/widget/leela-widget.min.js
