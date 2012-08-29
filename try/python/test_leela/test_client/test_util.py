affa1bb45dbcc4730579b76f120095dee19e401c
diego souza <dsouza@c0d3.xxx>
2012-08-29 00:03:27 -0300

    defining a very simple rate limit object

--- try/python/test_leela/test_client/test_util.py
+++ try/python/test_leela/test_client/test_util.py
@@ -13,22 +13,20 @@
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
-#    limitations under the License.
 
-import os
+import random
 import time
+from nose.tools import *
+from leela.client.utils import RateLimit
 
-def hostname():
-    return(os.uname()[1])
+def test_ratelimit_with_negative_ttls_expires_immediately():
+    rl = RateLimit()
+    rl.set("foobar", -1)
+    ok_(rl.expired("foobar"))
 
-class RateLimit(object):
-
-    def __init__(self):
-        self.memory = {}
-
-    def set(self, key, ttl):
-        self.memory[key] = (time.time(), ttl)
-
-    def expired(self, key):
-        (at, ttl) = self.memory.get(key, [0,0])
-        return (time.time() > at+ttl)
+def test_ratelimit_must_expires_according_to_ttl():
+    rl = RateLimit()
+    rl.set("foobar", 0.5)
+    ok_(not rl.expired("foobar"))
+    time.sleep(1)
+    ok_(rl.expired("foobar"))
